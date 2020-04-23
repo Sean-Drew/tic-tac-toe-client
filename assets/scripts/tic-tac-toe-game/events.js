8d0711e625efcefbd9d1f53ae89964b9b6acc839
Sean-Drew <SDrew989@gmail.com>
2020-04-23 14:26:03 -0400

    Work win condition logic
    Worked on logic behind checking for win conditions.

--- assets/scripts/tic-tac-toe-game/events.js
+++ assets/scripts/tic-tac-toe-game/events.js
@@ -18,20 +18,32 @@ const onTokenAdd = function (event) {
     // call game board function vv
     gameBoard[targetId] = currentPlayerToken
     // change players after a move vv
+    checkForWin()
     if (currentPlayerToken === 'x') {
       currentPlayerToken = "o"
     } else {
       currentPlayerToken = 'x'
-    }
+    } console.log(gameBoard)
   }
 }
 
-// identify win conditions
-// check game board after every move.
-// if a win condition is met, end game. If not, continue game.
-// AKA ^^ if win condition = true, end game. If win condition = false, continue.
-// use a while loop in here somehow? While win condition = false, continue game?
+const checkForWin = function () {
+  if (gameBoard[0] === currentPlayerToken && gameBoard[1] === currentPlayerToken && gameBoard[2] === currentPlayerToken) {
+    console.log('GAME OVER')
+  }
+}
 
+// identify win conditions (written down on paper right now).
+// check game board after every move. iterate through the game board? No, probably just need to call on specific combinations that equal a win condition?
+// if a win condition is met, end game. If not, continue game.
+// if (win condition === true) {
+//   end game
+// } else {
+//   continue game
+// }
+
+// end game, reset board
+// check if array is full, and no win condition is met. board full func?
 
 module.exports = {
   onTokenAdd
