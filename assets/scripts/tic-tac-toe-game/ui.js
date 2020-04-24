d6ccb1abc7eb35e27d5c67a8e35d6a32d9999e80
Sean-Drew <SDrew989@gmail.com>
2020-04-24 11:15:09 -0400

    Work UI messages
    Deleted line that was requiring events file into ui file, meant to
    delete that prior to last commit.

--- assets/scripts/tic-tac-toe-game/ui.js
+++ assets/scripts/tic-tac-toe-game/ui.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const store = require('../store')
-const events = require('./events')
 
 // add messaging for the user when the turn changes. can probably use currentPlayerToken from events.js if we require the file first? Going to try that - circular dependency? Got rid of line that was requiring events.js and code still functioned fine.
 // to get changeTurnSuccess to work, had to make it take in the player, and then when invoking changeTurnSuccess in events.js had to pass currentPlayerToken as the argument. Had to invoke changeTurnSuccess in both parts of the if...else statement governing changing player turn otherwise it just kept saying player o.
